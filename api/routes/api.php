d214c3ddf4bc4f3b4d486bbe3f2d8d2b55340014
avi <abhi.uk96@gmail.com>
2020-08-11 10:52:55 +0100

    Add subdomain for the api

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/weather', function (){
+    //dd("scalled");
+    $weather = [
+        'LONDON' => 'Sunny',
+        'MANCHESTER' => 'rain',
+        'ABERDEEN' => 'cold',
+    ];
+    return $weather;
 });
